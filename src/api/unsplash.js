bf4fef1ec06ef6778cb2b4660aec6fc7c2d21d71
Dheeraj <Dheeraj@Users-MacBook-Pro.local>
2019-03-13 17:55:11 -0500

    final commit

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ export default Axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
     Authorization:
-    'Client-ID 69a5705e2634ca6338f73b77d7eed5b19321211ab75c6736076079170a34ba99'
+    'Client-ID ' + process.env.REACT_APP_UNSPLASH_API_KEY
   }
 })
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,16 +12,21 @@ class SearchBar extends React.Component {
 
   render() {
     return (
-    <div className="ui segment">
+
+    <div  className="ui segment">
       <form onSubmit={this.onFormSubmit} className="ui form">
-        <div className="field">
-          <label>Image Search</label>
-          <input
-          type="text"
-          placeholder="what would you like to search?"
-          value={this.state.term}
-          onChange={e=>this.setState({ term:e.target.value})}
-          />
+        <div style={{display:'flex'}} className="field">
+            <label>Image Search</label>
+            <input
+            className="formfields"
+            type="text"
+            placeholder="what would you like to search?"
+            value={this.state.term}
+            onChange={e=>this.setState({ term:e.target.value})}
+            />
+          <div style={{marginLeft:'5px'}} class="ui icon primary button">
+            <i class="search icon"></i>
+          </div>
         </div>
       </form>
     </div>
