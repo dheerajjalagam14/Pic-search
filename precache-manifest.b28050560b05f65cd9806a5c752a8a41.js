466ca49236c3b3811d6ae8307389de0b94af404b
Dheeraj <Dheeraj@Users-MacBook-Pro.local>
2019-03-17 13:32:39 -0500

    Updates

---
+++ precache-manifest.b28050560b05f65cd9806a5c752a8a41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26bea828c1b5430d6c9d",
+    "url": "/Pic-search/static/js/runtime~main.069b55d3.js"
+  },
+  {
+    "revision": "3f6d893fa9e93324322f",
+    "url": "/Pic-search/static/js/main.cad56014.chunk.js"
+  },
+  {
+    "revision": "5d21101413ea9baee411",
+    "url": "/Pic-search/static/js/2.589c286c.chunk.js"
+  },
+  {
+    "revision": "3f6d893fa9e93324322f",
+    "url": "/Pic-search/static/css/main.5f92c453.chunk.css"
+  },
+  {
+    "revision": "043a0636ec1007ae7f029c2b8cef4cbf",
+    "url": "/Pic-search/index.html"
+  }
+];
--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import Axios from 'axios';
-
-export default Axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization:
-    'Client-ID ' + process.env.REACT_APP_UNSPLASH_API_KEY
-  }
-})
